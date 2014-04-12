6a45244918ca4bcf426726cc5cd792c3cd49ba15
Alejandro Raul Garcia <alejandroraulgarcia@gmail.com>
2014-04-12 11:22:23 -0300

    Primer commit
    Imégenes que nos dio el profesor.
    Se creo inicio.php con los div principales y el css con algunos estilos
    para darle forma a estos divs.

---
+++ inicio.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title></title>
+		<LINK rel="stylesheet" href="styles/inicio.css" type="text/css">
+	</head>
+	<body>
+		<div class="logged"></div>
+		<div class="logo"></div>
+		<div class="menu"></div>
+		<div class="content">
+			<div class="column column1"></div>
+			<div class="column column2"></div>
+		</div>
+		<div class="suggestions"></div>
+		<div class="footer"></div>
+	</body>
+</html>
+	
