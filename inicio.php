550e8679d498c31aa5fe56e227109e44eefd6574
Alejandro Raul Garcia <alejandroraulgarcia@gmail.com>
2014-04-13 22:47:03 -0300

    Maquetado
    Se maqueto hasta el login. Faltan cosas.

--- inicio.php
+++ inicio.php
@@ -4,15 +4,49 @@
 		<LINK rel="stylesheet" href="styles/inicio.css" type="text/css">
 	</head>
 	<body>
-		<div class="logged"></div>
-		<div class="logo"></div>
-		<div class="menu"></div>
-		<div class="content">
-			<div class="column column1"></div>
-			<div class="column column2"></div>
+		<div class="left"><div>
+		<div class="center">
+			<div class="logged"></div>
+			<div class="logo">
+				<img src="img/logoishidan.png" alt="Ishidan"/>
+			</div>
+			<div class="menu">
+				<div class="menuButton"><div class="menuText">INICIO</div></div>
+				<div class="menuButton"><div class="menuText">MENÚ</div></div>
+				<div class="menuButton"><div class="menuText">SUCURSALES</div></div>
+			</div>
+			<div class="content">
+				<div class="column column1">
+					<div class="title welcome"><span class="title">BIENVENIDO</span></div>
+					<div class="info">Consulte nuetro <a> men&uacute;</a> o ingrese su usuario y contrase&ntilde;a para hacer su pedido online.</div>
+					<div class="question">
+						<div class="questionImg"><img src="img/pregunta.png" /></div>
+						<div class="questionReg">Si a&uacute;n no se registr&oacute; haga click <a>aqu&iacute;</a> para iniciar su pedido</div>
+					</div>
+				</div>
+				<div class="middle"></div>
+				<div class="column column2">
+					<div class="title"><span class="title">INGRESAR</span><span class="subtitle">DELIVERY ONLINE</span></div>
+					<div class="loggin">
+						<div class="label">Numero de telefono</div>
+						<div class="input">
+							<input type="text"/>
+						</div>
+						<div class="label">Contraseña</div>
+						<div class="input">
+							<input type="text"/>
+						</div>
+						<div class="input">
+							<img src="img/boton.png"/>
+						</div>
+					</div>
+				</div>
+				<div class="middle"></div>
+			</div>
+			<div class="suggestions"></div>
+			<div class="footer"></div>
 		</div>
-		<div class="suggestions"></div>
-		<div class="footer"></div>
+		<div class="right"></div>
 	</body>
 </html>
 	
